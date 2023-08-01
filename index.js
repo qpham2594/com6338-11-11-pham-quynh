660151b3cff3e3e99a7eb7068e9f4d5fddd022d2
Quynh <qpham2594@gmail.com>
2023-08-01 17:14:45 -0400

    comments added

--- index.js
+++ index.js
@@ -14,21 +14,27 @@ const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 
 // complete this function
+// tried async and it became undefined, so no need to use async here since it's already applied with onclick function below
+// fetched API within this function initially and repeatedly getting [object Promise] since there was no promise return; already fetched with onclick
+
 const makePoemHTML = (poemApp) => {
   try {
-    //const poemApp = await getJSON(poemURL);
-    //const poemData = {tile, author, lines}
-   // return poemData
-    console.log(poemApp[0]);
-  
+    // makeTag for title of Poem
     const poemTitle = poemApp[0].title;
     const titleTag = makeTag('h2')(poemTitle);
-    console.log(titleTag)
-
+    
+    // makeTag for the author and em tags, used pipe here to have h3 wrapped around em (so em first follow by h3 in pipe), and then have content inside
     const author = poemApp[0].author;
     const emAndH3Tag = pipe(makeTag('em'), makeTag('h3'));
     const poemEmphasis = emAndH3Tag(`by ${author}`);
-    console.log(poemEmphasis)
+    
+    // Using reduce method  that takes a callback function and the initial value
+    // Object: stanzas and currentStanzas
+    // Two parameters: stanzaBlock - accumulator, and line - element being processed
+    // If the line is an empty string, then the stanza has done and it will join the lines with 'br' and wraps the stanza with 'p' tags
+    // Then that stanza gets pushed to the 'stanzas' array and the 'currentStanza' get resets to make new stanza
+    // If the line is not empty, then it gets added to the 'currentStanza' array
+    // Reduce() will repeat this until the linesArray is done and then wrap everything around 'p' tags
 
     const linesArray = poemApp[0].lines;
     const poemStanzas = linesArray.reduce((stanzaBlock, line) => {
@@ -45,20 +51,21 @@ const makePoemHTML = (poemApp) => {
       return stanzaBlock;
     }, { stanzas: [], currentStanza: [] });
     
-    // Handle the last stanza in case the poem doesn't end with an empty line
+    // Handling the last stanza in case the poem doesn't end with an empty line
+    // Join with 'br' and push the last stanza to into 'stanzas'
     if (poemStanzas.currentStanza.length > 0) {
       const stanzaContent = poemStanzas.currentStanza.join('<br>');
       poemStanzas.stanzas.push(`<p>${stanzaContent}</p>`);
     }
     
-    // Join the stanzas to form the whole poem
+    // Joining all of the stanzas to form the whole poem
     const wholePoem = poemStanzas.stanzas.join('');
     console.log(wholePoem)
 
-    // Set the innerHTML of poemEl directly
+    // Making a constant to combine tile, author, and lines together into one
+    // Return the constant to display on DOM, otherwise will be undefined.
     const wholeHTML = titleTag + poemEmphasis + wholePoem;
     return wholeHTML
-   // return poemApp[0]
   
   } catch (error) {
     console.log('unable to get data', error);
